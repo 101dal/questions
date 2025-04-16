ebf1196ef892385b6816f72e96d5e4e7840b132b
101dal <101dalcontact@gmail.com>
2025-04-16 21:00:08 +0200

    Remove test from default quiz

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const DEFAULT_QUIZ_URLS = [
-        "https://raw.githubusercontent.com/101dal/questions/refs/heads/main/quiz/test.json",
         "https://raw.githubusercontent.com/101dal/questions/refs/heads/main/quiz/chapitre%203.json"
     ];
 
